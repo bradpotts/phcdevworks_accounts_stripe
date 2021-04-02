539677faab1ea461416be5268b2c50fabdbf9cd9
BradPotts <brad.potts@phcnet.co>
2021-04-02 13:50:56 +0000

    add to the menu, adjusted layouts, fixes to views.

--- app/controllers/phcdevworks_accounts_stripe/admin/subscriptions_controller.rb
+++ app/controllers/phcdevworks_accounts_stripe/admin/subscriptions_controller.rb
@@ -1,6 +1,11 @@
 require_dependency "phcdevworks_accounts_stripe/application_controller"
 
 module PhcdevworksAccountsStripe
-  class Admin::SubscriptionsController < ApplicationController
-  end
+    class Admin::SubscriptionsController < ApplicationController
+
+        def admin_subscription_list
+            @admin_subscription_list = Stripe::Subscription.list({limit: 100})
+        end
+
+    end
 end
