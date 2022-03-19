690276bdf000c36b8cc43f88f2053c844eb868a0
BradPotts <brad.potts@phcnet.co>
2022-03-19 12:57:39 +0000

    fixes to views.

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,8 +1,8 @@
 require 'stripe'
 
 Rails.configuration.stripe = {
-  :publishable_key => ENV['PHCDEV_STRIPE_PK'],
-  :secret_key      => ENV['PHCDEV_STRIPE_SK']
+  :publishable_key => 'pk_test_51Hn30iCfVkks6MxhUN43ZasxdcUIDVX3q1PdfQneHhkvH1ybSHDzEifbvePCyliUKpe8IGeRNryWwWiiCsGERwA900qC3cyXwE',
+  :secret_key      => 'sk_test_51Hn30iCfVkks6MxhZTJ3PZmOXv5SPirttJBzdHyrZ9q4M7cYi5J4I5EL5DDfxJbl0CWMopVSltvcrR0FYhGIepXM00zqpjJAxD'
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
